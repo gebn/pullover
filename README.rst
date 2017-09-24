c49150050971e6a8f923b003be2ce627dcfc54b6
George Brighton <george@gebn.co.uk>
2017-09-24 16:22:02 +0100

    Add skeleton

---
+++ README.rst
@@ -0,0 +1,40 @@
+pullover
+========
+
+The simplest Pushover API wrapper for Python.
+
+Features
+--------
+
+- No extraneous requests - just sends non-emergency messages quickly and without fuss
+- Aims to get the basics right, and be open to extension for more advanced use cases
+- Timeouts and automatic back-off should Pushover be experiencing issues
+- Intuitive command-line interface with sane, parseable output
+- Unit and integration tested
+- Signed PyPi releases
+
+Installation
+------------
+
+::
+
+    $ pip install pullover
+
+Demo
+----
+
+.. code-block:: python
+
+    from pullover import Application, User, Message, PulloverError
+
+    try:
+        aws = Application('57tn23v578n9887nvh2g5892')
+        george = User('8v57nhg578hh5n0h887hh04245')
+        message = Message('foo bar', title='hello')
+        message.send(aws, george)
+    except ValueError:
+        # invalid argument
+        pass
+    except PulloverError:
+        # sending error
+        pass
---
+++ setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from setuptools import setup, find_packages
+import codecs
+
+
+def _read_file(name, encoding='utf-8'):
+    """
+    Read the contents of a file.
+
+    :param name: The name of the file in the current directory.
+    :param encoding: The encoding of the file; defaults to utf-8.
+    :return: The contents of the file.
+    """
+    with codecs.open(name, encoding=encoding) as f:
+        return f.read()
+
+
+setup(
+    name='pullover',
+    version='0.0.1',
+    description='The simplest Pushover API wrapper for Python.',
+    long_description=_read_file('README.rst'),
+    license='MIT',
+    url='https://github.com/gebn/pullover',
+    author='George Brighton',
+    author_email='oss@gebn.co.uk',
+    packages=find_packages(),
+    zip_safe=True,
+    install_requires=[
+        'backoff',
+        'requests',
+        'six>=1.9.0'
+    ],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+        'mock'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
