e72d4de3213846bd2fd8b70f01f6e22248a7bd2f
George Brighton <george@gebn.co.uk>
2017-09-25 21:14:37 +0100

    Implement non-retry-related Message tests

--- pullover/tests/test_message.py
+++ pullover/tests/test_message.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import six
 import unittest
-
+import datetime
+import pytz
 import responses
 import requests
+from six.moves import urllib_parse
 
+import pullover
+from pullover import Application, User
 from pullover.message import ClientSendError, ServerSendError, SendResponse, \
     Message
 
@@ -75,3 +80,56 @@ class TestSendResponse(unittest.TestCase):
     def test_raise_for_status_ok(self):
         SendResponse(self._response(json=self._SUCCESS_JSON)) \
             .raise_for_status()
+
+
+class TestMessage(unittest.TestCase):
+
+    _BODY = 'hello'
+    _TITLE = 'title'
+    _TIMESTAMP = pytz.utc.localize(datetime.datetime.utcnow()) - \
+        datetime.timedelta(minutes=5)
+    _EPOCH_SECONDS = int((_TIMESTAMP - Message._EPOCH_START).total_seconds())
+    _URL = 'https://gebn.co.uk'
+    _PRIORITY = Message.HIGH
+    _MESSAGE = Message(_BODY)
+    _APP_TOKEN = 'foo'
+    _APP = Application(_APP_TOKEN)
+    _USER_KEY = 'bar'
+    _USER = User(_USER_KEY)
+
+    @responses.activate
+    def test_send_user_agent(self):
+        # ensure user agent is correctly set in request
+        def callback(request):
+            self.assertEqual(request.headers['User-Agent'],
+                             '{0}/{1}'.format(pullover.__title__,
+                                              pullover.__version__))
+            return 200, {}, ''
+
+        responses.add_callback(responses.POST, Message._ENDPOINT,
+                               callback=callback)
+
+        self._MESSAGE.send(self._APP, self._USER)
+
+    @responses.activate
+    def test_send_user_fields(self):
+        def callback(request):
+            params = urllib_parse.parse_qs(request.body)
+            self.assertEqual(request.method, 'POST')
+            self.assertEqual(params['token'][0], self._APP_TOKEN)
+            self.assertEqual(params['user'][0], self._USER_KEY)
+            self.assertEqual(params['message'][0], self._BODY)
+            self.assertEqual(params['title'][0], self._TITLE)
+            self.assertEqual(params['timestamp'][0], str(self._EPOCH_SECONDS))
+            self.assertEqual(params['url'][0], self._URL)
+            self.assertEqual(params['priority'][0], str(self._PRIORITY))
+            return 200, {}, ''
+
+        responses.add_callback(responses.POST, Message._ENDPOINT,
+                               callback=callback)
+
+        Message(self._BODY, self._TITLE, self._TIMESTAMP, self._URL,
+                self._PRIORITY).send(self._APP, self._USER)
+
+    def test_str(self):
+        self.assertEqual(str(self._MESSAGE), 'Message({0})'.format(self._BODY))
