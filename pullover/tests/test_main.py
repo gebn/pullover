eac373e14afd5d7c22b2edc90f4067fb834ae2d0
George Brighton <george@gebn.co.uk>
2017-09-30 23:11:07 +0100

    Implement proper tests for EnvDefault argparse action

--- pullover/tests/test_main.py
+++ pullover/tests/test_main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import argparse
 import unittest
 import functools
 import datetime
@@ -13,12 +14,6 @@ from pullover import __main__ as main, Message
 from pullover.__main__ import EnvDefault
 
 
-class TestEnvDefault(unittest.TestCase):
-    def test_no_env(self):
-        with self.assertRaises(ValueError):
-            EnvDefault(None)
-
-
 @contextlib.contextmanager
 def _suppress_stderr():
     save_stderr = sys.stderr
@@ -28,6 +23,38 @@ def _suppress_stderr():
     finally:
         sys.stderr.close()
         sys.stderr = save_stderr
+
+
+class TestEnvDefault(unittest.TestCase):
+
+    # N.B. environment is sampled in add_argument() - cannot call this outside
+    #      of test
+
+    def test_no_env(self):
+        with self.assertRaises(ValueError):
+            EnvDefault(None)
+
+    def test_no_env_defined(self):
+        with self.assertRaises(SystemExit), _suppress_stderr():
+            parser = argparse.ArgumentParser()
+            parser.add_argument('-a', action=EnvDefault, env='SOMETHING')
+            parser.parse_args([])
+
+    @mock.patch.dict(os.environ, {'SOMETHING': 'envvalue'})
+    def test_env_defined(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-a', action=EnvDefault, env='SOMETHING')
+        namespace = parser.parse_args([])
+        self.assertEqual(namespace.a, 'envvalue')
+
+    @mock.patch.dict(os.environ, {'SOMETHING': 'envvalue'})
+    def test_precedence(self):
+        # explicit value (foo) should always take precedence over implicit
+        # value (envvalue)
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-a', action=EnvDefault, env='SOMETHING')
+        namespace = parser.parse_args(['-a', 'foo'])
+        self.assertEqual(namespace.a, 'foo')
 
 
 # TODO make this default, and only require a wrapper for when these *aren't*
