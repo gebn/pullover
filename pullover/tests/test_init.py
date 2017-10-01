21c13604a5c1ac2301b1818a222e1e9b637b5cad
George Brighton <george@gebn.co.uk>
2017-10-01 01:59:01 +0100

    Attempt to implement tests for importing pullover when it is not installed

--- pullover/tests/test_init.py
+++ pullover/tests/test_init.py
@@ -1,11 +1,45 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import unittest
+import mock
+import sys
 import responses
 
 import pullover
 from pullover import Message
 from pullover.tests import test_message
+
+
+class TestInstallStatus(unittest.TestCase):
+
+    @unittest.skipUnless(sys.version_info.major == 2,
+                         'Only applies to Python 2')
+    @mock.patch('os.path.normcase')
+    def test_uninstalled_2(self, normcase):
+        normcase.return_value = 'rubbish'
+        reload(pullover)
+        self.assertEqual(pullover.__version__, 'unknown')
+
+    @unittest.skipUnless(sys.version_info.major == 3 and
+                         (sys.version_info.minor == 2 or
+                          sys.version_info.minor == 3),
+                         'Only applies to Python 3.2 and 3.3')
+    @mock.patch('os.path.normcase')
+    def test_uninstalled_32_33(self, normcase):
+        import imp
+        normcase.return_value = 'rubbish'
+        imp.reload(pullover)
+        self.assertEqual(pullover.__version__, 'unknown')
+
+    @unittest.skipUnless(sys.version_info.major == 3 and
+                         sys.version_info.minor >= 4,
+                         'Only applies to Python 3.4+')
+    @mock.patch('os.path.normcase')
+    def test_uninstalled_34_plus(self, normcase):
+        import importlib
+        normcase.return_value = 'rubbish'
+        importlib.reload(pullover)
+        self.assertEqual(pullover.__version__, 'unknown')
 
 
 class TestSend(unittest.TestCase):
